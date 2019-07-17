c4c8f8019519f6b8903c521065f004791d313b84
Ben <allenben@protonmail.com>
2019-07-17 16:43:19 -0500

    Mounted all components to App.js

--- src/App.js
+++ src/App.js
@@ -9,11 +9,14 @@ import "./App.css";
 
 
 function App() {
+
+ 
+
   return (
-    <div>
+    <div class="App">
       <Header />
       <Photo />
-      <Message />
+      <Message/>
     </div>
   )
 }
--- src/components/Photo.js
+++ src/components/Photo.js
@@ -1,11 +1,32 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const Photo = () => {
+
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        axios
+        .get(
+            "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=2019-07-15"
+        )
+            .then(fetch => {
+                console.log(fetch.data.hdurl)
+                setData(`${fetch.data.hdurl}`)
+                console.log("Data fetched successfully.")
+            }
+            )
+            .catch(() =>
+            console.log('Error in fetching server data.'))
+            }, [])
+
     return (
-      <div>
-        <img src="" alt="Photo of the Day."></img>
+      <div >
+        <img className="photo" src={data} alt="Photo of the Day."></img>
       </div>
     );
+
+    
   };
 
 export default Photo;
